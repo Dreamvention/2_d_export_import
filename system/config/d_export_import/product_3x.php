36c82ec6db7120ec6a66b57b08b81e5ea703f265
dfalcon <php.drvn@gmail.com>
2019-08-07 13:38:48 +0300

    sem var

--- system/config/d_export_import/product_3x.php
+++ system/config/d_export_import/product_3x.php
@@ -4,7 +4,9 @@ $_['opencart_version'] = array(
     "3.0.1.1",
     "3.0.1.2",
     "3.0.2.0",
-    "3.0.3.1");
+    "3.0.3.1",
+    "3.0.3.2",
+    "3.0.3.3");
 $_['main_sheet'] = array(
     'name' => 'Products',
     'event_export' => array(
